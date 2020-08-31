faba90d017aaf9d8b41ab5d909e9bf3ecf2dfaea
nagyrajmund <rajmund977@gmail.com>
2020-09-01 01:29:27 +0200

    Rework the testing script 'test.py'

--- gesticulator/test.py
+++ gesticulator/test.py
@@ -1,94 +1,63 @@
-"""
-This script generates gestures output based on the speech input.
-The gestures will be written in the text file:
-3d coordinates together with the velocities.
-Apart from that, video of the gesture will be generated.
-
-author: Taras Kucherenko
-contact: tarask@kth.se
-"""
-
-import torch
-import numpy as np
-
+from os.path import isfile, join
 from gesticulator.model.model import GesticulatorModel
-from gesticulator.data_processing.SGdataset import SpeechGestureDataset
+from config.model_config import construct_model_config_parser
+from pytorch_lightning import Trainer
 
-# Params
-from gesticulator.parameters import parser
+def main(test_params):
+    model = load_model(test_params)
+    trainer = Trainer.from_argparse_args(test_params, logger=False)
 
-torch.set_default_tensor_type('torch.FloatTensor')
+    trainer.test(model)
 
+def load_model(test_params):
+    """This function enables the test datasets that were selected by the user"""
+    model = GesticulatorModel.load_from_checkpoint(test_params.model_file, inference_mode=True)
+    
+    # Make sure that at least one of the two test datasets are enabled
+    if not test_params.test_semantic and not test_params.test_random:
+        print("ERROR: Please provide at least one of the following two flags:")
+        print("       python test.py --test_semantic (to use the semantic test input segments)")
+        print("       python test.py --test_random (to use the random test input segments)")
+        exit(-1)
 
-def predict(model, speech_file, text_file, gesture_file):
-    """ Predict human gesture based on the speech
+    model.hparams.generate_semantic_test_predictions = test_params.test_semantic
+    model.hparams.generate_random_test_predictions = test_params.test_random
+    
+    return model
 
-    Args:
-        mean_pose:     mean_pose in the dataset
-        model:       model to evaluate
-        speech_file:    address to the speech file
-        text_file:      address to the text file
-        gesture_file:   file to save gestures in
+def add_test_script_arguments(parser):
+    parser.add_argument('--test_semantic', '-semantic', action="store_true",
+                        help="If set, test the model with the semantic input segments")
 
-    Returns:
-        nothing, saves the gesture into a txt file
+    parser.add_argument('--test_random', '-random', action="store_true",
+                        help="If set, test the model with the random input segments")
+    
+    parser.add_argument('--model_file', '-model', help="Path to the model checkpoint to be loaded")
 
-    """
-
-    model.eval()
-
-    test_length = 300
-
-    S = np.load(speech_file)[:test_length]
-    S_tensor = torch.tensor(S).float()
-
-    T = np.load(text_file)[:int(test_length/2)]
-    T_tensor = torch.tensor(T).float()
-    # upsample text to get the same sampling rate as the audio
-    cols = np.linspace(0, int(test_length/2), endpoint=False, num=test_length, dtype=int)
-    T_tensor = T_tensor[cols, :]
-
-    # create a "batch" and then take the first element of the resulting batch
-    motion_seq = model.forward(S_tensor.unsqueeze(0), T_tensor.unsqueeze(0)).squeeze()
-
-    # detach
-    gestures_norm = np.array(motion_seq.detach().numpy())
-
-    # denormalize
-    gestures = gestures_norm * model.max_val + model.mean_pose[np.newaxis]
-    print(gestures.shape)
-
-    np.save(gesture_file, gestures)
-
+    return parser
 
 if __name__ == "__main__":
-
+    """
+    Usage: 
+        python test.py (--model_file MODEL_FILE | --run_name RUN_NAME ) [--test_semantic] [--test_random]
+    """
+    # Model parameters are added here
+    parser = construct_model_config_parser()
+    parser = add_test_script_arguments(parser)
     args = parser.parse_args()
-
-    mean_pose = np.array([0 for i in range(45)])
-    max_val = np.array([0 for i in range(45)])
-
-    the_model = GesticulatorModel(args, mean_pose, max_val)
-    the_model.load_state_dict(torch.load(args.model_file))
-
-    train_dataset = SpeechGestureDataset(args.data_dir, train=True, apply_PCA=args.pca)
-
-    # Produce gestures
-    print("Generation gestures ...")
-
-    gesture_file = "temp_ges.npy"
-    predict(the_model, args.test_audio, args.test_text, gesture_file)
-
-
-    """print("Making a video ... ")
-    epoch = args.curr_epoch
-
-    # define files
-    resulting_bvh_file = "../log/gestures/result_ep" + str(epoch) + '.bvh'
-    resulting_npy_file = "../log/gestures/result_ep" + str(epoch) + '.npy'
-    resulting_video_file = "../log/gestures/result_ep" + str(epoch) + '.mp4'
-
-    gestures = np.load(gesture_file)
-
-    visualize(gestures, resulting_bvh_file, resulting_npy_file, resulting_video_file, start_t=0, end_t=20)"""
-
+    
+    # If the model file is not provided, look for the trained checkpoint 
+    # in the results/RUN_NAME directory
+    if not args.model_file:
+        last_ckpt = join(args.result_dir, args.run_name, "trained_model.ckpt")
+        
+        if isfile(last_ckpt):
+            args.model_file = last_ckpt
+        else:
+            print("ERROR: Please provide the saved model checkpoint with:")
+            print("         python test.py --model_file PATH_TO_CHECKPOINT\n")
+            print("       Alternatively, you can provide the run_name argument you set during training:")
+            print("         python test.py --run_name RUN_NAME")
+            exit(-1)
+    
+    main(args)
