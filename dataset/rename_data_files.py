ea632e70ca01813a97856d75d9dc962ad20d0606
nagyrajmund <rajmund977@gmail.com>
2021-02-15 17:39:10 +0100

    Use os.path.join() instead of path concatenation in rename_data_files.py

--- dataset/rename_data_files.py
+++ dataset/rename_data_files.py
@@ -1,13 +1,14 @@
 import os
+from os.path import join
 import csv
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
     #  Setup parameter parser
     parser = ArgumentParser()
-    parser.add_argument('--folder_downloaded', '-f_down', required=True,
-                                   help="Path where the downloaded GENEA dataset is stored. Should end with `GENEA_Challenge_2020_data_release`.")
-    parser.add_argument('--folder_renamed', '-f_renamed',  default="./raw_data/",
+    parser.add_argument('--folder_downloaded', '-f_down', default="./genea_data",
+                                   help="Path where the GENEA dataset is stored, downloaded from the `GENEA_Challenge_2020_data_release` folder.")
+    parser.add_argument('--folder_renamed', '-f_renamed',  default="./raw_data",
                                    help="Path where renamed dataset will be stored")
 
     params = parser.parse_args()
@@ -18,7 +19,7 @@ if __name__ == '__main__':
 
     # TRAIN
     # Go though the indices from the GENEA training set and rename files for those in the original dataset
-    train_folder = params.folder_downloaded + "/Training_data/"
+    train_folder = join(params.folder_downloaded, "Training_data")
     with open('Data_renaming_indices.csv') as csvfile:
          csv_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
          headers = next(csv_reader, None)
@@ -32,30 +33,30 @@ if __name__ == '__main__':
              # rename audio files
              genea_file_name = "Recording_"+str(genea_id).zfill(3)+".wav"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".wav"
-             audio_genea_folder = train_folder + "Audio/"
-             audio_original_folder = folder_processed + "/Audio/"
+             audio_genea_folder = join(train_folder, "Audio")
+             audio_original_folder = join(folder_processed, "Audio")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
              # rename motion files
              genea_file_name = "Recording_"+str(genea_id).zfill(3)+".bvh"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".bvh"
-             audio_genea_folder = train_folder + "/Motion/"
-             audio_original_folder = folder_processed + "/Motion/"
+             audio_genea_folder = join(train_folder, "Motion")
+             audio_original_folder = join(folder_processed, "Motion")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
              # rename text files
              genea_file_name = "Recording_"+str(genea_id).zfill(3)+".json"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".json"
-             audio_genea_folder = train_folder + "/Transcripts/"
-             audio_original_folder = folder_processed + "/Transcripts/"
+             audio_genea_folder = join(train_folder, "Transcripts")
+             audio_original_folder = join(folder_processed, "Transcripts")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
     # TEST
     # Go though the indices from the GENEA test set and rename files for those in the original dataset
-    test_folder =  params.folder_downloaded  + "/Test_data/"
+    test_folder = join(params.folder_downloaded, "Test_data")
     with open('Data_renaming_indices.csv') as csvfile:
          csv_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
          headers = next(csv_reader, None)
@@ -72,25 +73,25 @@ if __name__ == '__main__':
              # rename audio files
              genea_file_name = "TestSeq"+str(genea_id).zfill(3)+".wav"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".wav"
-             audio_genea_folder = test_folder + "Audio/"
-             audio_original_folder = folder_processed + "/Audio/"
+             audio_genea_folder = join(test_folder, "Audio")
+             audio_original_folder = join(folder_processed, "Audio")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
              # rename motion files
              genea_file_name = "TestSeq"+str(genea_id).zfill(3)+".bvh"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".bvh"
-             audio_genea_folder = test_folder + "/Motion/"
-             audio_original_folder = folder_processed + "/Motion/"
+             audio_genea_folder = join(test_folder, "Motion")
+             audio_original_folder = join(folder_processed, "Motion")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
              # rename text files
              genea_file_name = "TestSeq"+str(genea_id).zfill(3)+".json"
              original_file_name = "NaturalTalking_"+str(orig_id).zfill(3)+".json"
-             audio_genea_folder = test_folder + "/Transcripts/"
-             audio_original_folder = folder_processed + "/Transcripts/"
+             audio_genea_folder = join(test_folder, "Transcripts")
+             audio_original_folder = join(folder_processed, "Transcripts")
 
-             os.rename(os.path.join(audio_genea_folder, genea_file_name), os.path.join(audio_original_folder, original_file_name))
+             os.rename(join(audio_genea_folder, genea_file_name), join(audio_original_folder, original_file_name))
 
     print("Done! The dataset with proper naming is stored at " + folder_processed)
